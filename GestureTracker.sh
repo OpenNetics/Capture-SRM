ba48d69e56db729f76d1251c2b76c47bc284dbab
karsh <utkarsh@cyberfear.com>
2025-12-14 12:38:03 +1100

    build: flag handlings and fixes
    ~ fixed the behaviour with flags and run. dont run the program when
      --update flag is used, for example

--- GestureTracker.sh
+++ GestureTracker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#- Helper Functions --------------------------------------------------------------------------------
+
 function print_help() {
     echo -e "USAGE:\n\033[31m  $0 \033[32m{OPTIONS}\n"
 
@@ -11,53 +13,71 @@ function print_help() {
     echo -e "\033[0m"
 }
 
+function create_virtual_env() {
+    echo -e "\033[33m[Creating a new virtual environment]\033[0m"
+    python3 -m venv ./.venv
 
-# Check for flags
+    echo -e "\033[33m[Installing requirements]\033[0m"
+    source ./.venv/bin/activate
+    python3 -m pip install -r ./src/requirements.txt
+}
+
+function delete_virtual_env() {
+    echo -e "\033[31m[Removing existing virtual environment]\033[0m"
+    rm -rf ./.venv
+}
+
+
+#- Check for Flags ---------------------------------------------------------------------------------
+
 case $1 in
     --install)
-        echo -e "\033[31m[Activating virtual environment]\033[0m"
-        source ./.venv/bin/activate 2>/dev/null
+        create_virtual_env
         ;;
 
     --reinstall)
-        echo -e "\033[31m[Removing existing virtual environment]\033[0m"
-        rm -rf ./.venv
-
-        echo -e "\033[31m[Creating a new virtual environment]\033[0m"
-        python3 -m venv ./.venv
-
-        echo -e "\033[31m[Installing requirements]\033[0m"
-        source ./.venv/bin/activate
-        python3 -m pip install -r ./src/requirements.txt
+        delete_virtual_env
+        create_virtual_env
         ;;
 
     --update)
-        if [ ! -d "./.venv" ]; then
-            echo -e "\033[31m[Virtual environment not found. Creating it]\033[0m"
-            python3 -m venv ./.venv
+        # pull new release
+        echo -e "\033[33m[Pulling updated git release]\033[0m"
+        git pull
 
-            echo -e "\033[31m[Installing requirements]\033[0m"
-            source ./.venv/bin/activate
-            python3 -m pip install -r ./src/requirements.txt
+        # activate virtual-environment
+        if [ ! -d "./.venv" ]; then
+            echo -e "\033[31m[Virtual environment not found]\033[0m"
+            create_virtual_env
+
         else
-            echo -e "\033[31m[Activating existing virtual environment]\033[0m"
+            echo -e "\033[33m[Activating existing virtual environment]\033[0m"
             source ./.venv/bin/activate
         fi
 
-        echo -e "\033[31m[Pulling updated git release]\033[0m"
-        git pull
-
-        echo -e "\033[31m[Updating installed packages]\033[0m"
+        # update all packages
+        echo -e "\033[33m[Updating installed packages]\033[0m"
         python3 -m pip install --upgrade -r ./src/requirements.txt
         ;;
 
     --help)
         print_help
-        exit 0
         ;;
 esac
 
-# Run the main Python script
-echo -e "\033[31m[Running GestureTracker]\033[0m"
-python3 ./src/main.py
+
+#- Run GestureTracker ------------------------------------------------------------------------------
+
+# Run the main script when no --args are added
+if [ -z "$1" ]; then
+    echo -e "\033[33m[Activating virtual environment]\033[0m"
+    if [ ! -d "./.venv" ]; then
+        echo -e "\033[31m[Virtual environment not found]\033[0m"
+        create_virtual_env
+    fi
+    source ./.venv/bin/activate 2>/dev/null
+
+    echo -e "\033[1;33m[Running GestureTracker]\033[0m"
+    python3 ./src/main.py
+fi
 
